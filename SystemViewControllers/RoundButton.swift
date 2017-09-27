d14c10f98f20766b94843ff169f8467f01ba8343
AA1 <aa1@AA1s-Mac.local>
2017-09-27 00:19:44 -0300

    ImageView included. Also the Segmented control was included.

---
+++ SystemViewControllers/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  SystemViewControllers
+//
+//  Created by AA1 on 26/09/17.
+//  Copyright © 2017 AA1. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = self.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
