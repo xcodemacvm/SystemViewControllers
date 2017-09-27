4add7c5a3e994ed7991dedcdd68dbe7290ed6956
AA1 <aa1@AA1s-Mac.local>
2017-09-27 01:50:10 -0300

    Added IBOutlet and IBAction.

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
 
+    @IBAction func segmentedControlOptionTapped(_ sender: UISegmentedControl) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
